3cf3939037813677912bef568e6d1a4465a92cc9
Verhovtsov Denis <verhovtsov.denis@mail.ru>
2025-01-03 00:18:17 +1000

    del pacal code from form

--- translator_project/main/forms.py
+++ translator_project/main/forms.py
@@ -4,13 +4,12 @@ from django import forms
 class TextForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea(
         attrs={
-            'class': "form-control",
-            'rows': 10,
+            'class': "form-control prettyprint",
+            'rows': 15,
             "placeholder": "Введите ваш код на Pascal..."}),
         label='',
-        initial='''pascal aaa;
-var a: read;
-begin
-a := 2;
-end.''',
+
+
         max_length=5000)
+
+
