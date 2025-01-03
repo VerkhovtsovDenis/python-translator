741efc541d765c19494d91ddfb5fe7d43ae59c1d
Verhovtsov Denis <verhovtsov.denis@mail.ru>
2025-01-03 16:40:45 +1000

    change form height

--- translator_project/main/forms.py
+++ translator_project/main/forms.py
@@ -1,15 +1,11 @@
 from django import forms
 
 
-class TextForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea(
+class PascalCodeFrom(forms.Form):
+    pascal_code = forms.CharField(widget=forms.Textarea(
         attrs={
             'class': "form-control prettyprint",
-            'rows': 15,
+            'rows': "15",
             "placeholder": "Введите ваш код на Pascal..."}),
         label='',
-
-
         max_length=5000)
-
-
